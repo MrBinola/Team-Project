9bd5ccb82673e188812b1d714f003f0b5136bd27
Code Wizard <codewizard@gmail.com>
2024-11-20 18:31:00 +0200

    Jake

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-console.log("I'm MrBinola");
-console.log("Miros BMW");
-console.log("I'm MrBinola")
-
-let r = 5
-let e = 995
-console.log(r + e)
-
-let mirosant = 10
-let nm = 10
-console.log(nm + mirosant)
+class unit{
+    name = "Night"
+    age = 6024
+}
